a05bd115d9d10f81e1df568f5d4d03320f7dce31
cok2exe <cok2exe@gmail.com>
2020-06-14 17:39:42 +0900

    add :) PrivateRoute 추가

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,17 +1,45 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Redirect, Route, RouteComponentProps, RouteProps, Switch } from 'react-router-dom'
 import Login from './pages/login'
 import App from './App'
 
-export default function Routes() {
+// 참고: https://eunajjing.github.io/2019/06/04/router/
+type RoutePageComponent =
+  | React.ComponentType<RouteComponentProps<any>>
+  | React.ComponentType<any>
+
+interface IPrivateRouteProps {
+  page: RoutePageComponent
+}
+
+const Routes:React.FC = () => {
+  const PrivateRoute:React.FC<IPrivateRouteProps & RouteProps> = ({ page: Component, ...rest }) => <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem('lol_nickname') ? (
+        <Component {...props} />
+      ) : <Redirect
+        to={{
+          pathname: "/login",
+          state: { from: props.location }
+        }}
+      />
+    }
+  />
+
   return (
     <Router>
-      <Route exact path="/login">
-        <Login />
-      </Route>
-      <Route exact path="/">
-        <App />
-      </Route>
+      <Switch>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <PrivateRoute exact path="/" page={App} />
+        {/*<PrivateRoute exact path="/">*/}
+        {/*  <App />*/}
+        {/*</PrivateRoute>*/}
+      </Switch>
     </Router>
   )
 }
+
+export default Routes
