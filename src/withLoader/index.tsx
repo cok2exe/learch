1876d48ba7486ebd8448c87a2cf96128465f7e27
cok2exe <cok2exe@gmail.com>
2020-11-22 11:41:13 +0900

    refactor :p common css

--- src/withLoader/index.tsx
+++ src/withLoader/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import { flex } from '../styles/values'
 
 const PoroLoader = styled.div`
   position: fixed;
@@ -8,9 +9,7 @@ const PoroLoader = styled.div`
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.8);
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  ${flex}
 `
 
 export default function withLoader (WrappedComponent: React.FC) {
