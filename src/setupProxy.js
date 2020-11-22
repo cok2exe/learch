de41222d76b7aea5635e7c30ca1d242cc062eedf
cok2exe <cok2exe@gmail.com>
2020-11-22 10:18:25 +0900

    fix :) api version

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/data',
     createProxyMiddleware({
-      target: "http://ddragon.leagueoflegends.com/cdn/10.12.1/",
+      target: `http://ddragon.leagueoflegends.com/cdn/${process.env.REACT_APP_VERSION}/`,
       changeOrigin: true
     })
   )
