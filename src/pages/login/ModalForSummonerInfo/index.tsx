a3aba4ec076d2fefbf9a38ad79aa0843801bb750
cok2exe <cok2exe@gmail.com>
2020-11-22 11:02:22 +0900

    style :) Dialog 활용 소환사 정보 팝업 개선

--- src/pages/login/ModalForSummonerInfo/index.tsx
+++ src/pages/login/ModalForSummonerInfo/index.tsx
@@ -1,22 +1,66 @@
 import React from 'react'
-import Modal from '../../../components/Modal'
+import {
+  Avatar,
+  Badge,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  Typography,
+} from '@material-ui/core'
 import { ISummonerInfo } from '../../../interface/summoner'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 
 interface IModalForSummonerInfoProps {
+  visible: boolean
   summonerInfo: ISummonerInfo
   profileIconSrc: string
-  onHide: () => void
+  onClose: () => void
 }
 
-const ModalForSummonerInfo:React.FC<IModalForSummonerInfoProps> = ({ summonerInfo, profileIconSrc, onHide }) => {
-  const { name, summonerLevel, profileIconId } = summonerInfo
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    large: {
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
+  }),
+)
+const ModalForSummonerInfo: React.FC<IModalForSummonerInfoProps> = ({
+  visible,
+  summonerInfo,
+  profileIconSrc,
+  onClose,
+}) => {
+  const { name, summonerLevel } = summonerInfo
+  const classes = useStyles()
 
   return (
-    <Modal title="소환사 정보" onHide={onHide}>
-      <p>{name}</p>
-      <p>{summonerLevel}</p>
-      <img src={profileIconSrc} alt={`${profileIconId}`}/>
-    </Modal>
+    <Dialog open={visible} onClose={onClose} fullWidth maxWidth="xs">
+      <DialogTitle>소환사 정보</DialogTitle>
+      <DialogContent dividers>
+        <Grid container spacing={2} direction="column" justify="center" alignItems="center">
+          <Grid item>
+            <Badge color="secondary" badgeContent={summonerLevel} max={9999}>
+              <Avatar className={classes.large} variant="square" src={profileIconSrc} />
+            </Badge>
+          </Grid>
+          <Grid item>
+            <Typography>{name}</Typography>
+          </Grid>
+        </Grid>
+      </DialogContent>
+      <DialogActions>
+        <Button color="primary" variant="contained" disableElevation>
+          LEARCH!
+        </Button>
+        <Button onClick={onClose} variant="contained" disableElevation>
+          닫기
+        </Button>
+      </DialogActions>
+    </Dialog>
   )
 }
 
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,59 @@
-import React from 'react'
-import FormForLogin from './FormForLogin'
+import React, { useState } from 'react'
 import styles from './index.module.scss'
+import { TextField } from '@material-ui/core'
+import { ILoadingFunction } from '../../interface/common'
+import ModalForSummonerInfo from './ModalForSummonerInfo'
+import withLoader from '../../withLoader'
+import { Summoner } from '../../api'
 
-const Login:React.FC = () => {
+const Login: React.FC<ILoadingFunction> = ({ setLoading }) => {
+  const [name, setName] = useState('')
+  const [summonerInfo, setSummonerInfo] = useState({})
+  const [profileIconSrc, setProfileIconSrc] = useState('')
+  const [visible, setVisible] = useState(false)
+
+  const getSummonerInfo = async (e: { preventDefault: () => void }) => {
+    try {
+      setLoading(true)
+      e.preventDefault()
+      const summonerInfo = await Summoner.getSummonerInfo(name)
+      setSummonerInfo(summonerInfo)
+      setProfileIconSrc(
+        `http://ddragon.leagueoflegends.com/cdn/${process.env.REACT_APP_VERSION}/img/profileicon/${summonerInfo.profileIconId}.png`,
+      )
+
+      setVisible(true)
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <div className={styles.login}>
-      <FormForLogin />
+      <form className={styles.form} autoComplete="off" onSubmit={getSummonerInfo}>
+        <img src={`${process.env.PUBLIC_URL}/images/lol_logo.png`} alt="league of legends" />
+        <TextField
+          label="롤 닉네임"
+          id="id"
+          name="id"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          variant="outlined"
+          fullWidth
+        />
+      </form>
+
+      <ModalForSummonerInfo
+        visible={visible}
+        summonerInfo={summonerInfo}
+        profileIconSrc={profileIconSrc}
+        onClose={() => setVisible(false)}
+      />
     </div>
   )
 }
 
-export default Login
+// @ts-ignore
+export default withLoader(Login)
